fixed gradle code and made more efficient. added EF calculation method

testing/gradio-app.py
@@ -60,7 +60,7 @@ else:
 
 model.eval()
 
-def segment(inp):
+def createSegmentation(inp, imagePath):
     x = inp.transpose([2, 0, 1])  #  channels-first
     x = np.expand_dims(x, axis=0)  # adding a batch dimension    
     
@@ -85,8 +85,10 @@ def segment(inp):
     rescaled = (255.0 / mask.max() * (mask - mask.min())).astype(np.uint8)
 
     im = Image.fromarray(rescaled)
-    imagePath = "testing/segmented_img.png"
     im.save(imagePath)
+
+def drawLines(frame, imagePath):
+    createSegmentation(frame, imagePath) # create frame1 segmentation
 
     volumes, x1, y1, x2, y2, degrees = funcs.calculateVolume(imagePath, 20, 1, "Method of Disks")
     image = cv2.imread(imagePath) # read in the image from the specified frame path
@@ -100,19 +102,37 @@ def segment(inp):
         
     cv2.line(image, longLine1stCoords, longLine2ndCoords, (255, 255, 255), 1) # Drawing the perpendicular/long line in different color
 
-    volume_data = "Volume: " + str(volumes[0])
+    return image, volumes[0]
 
-    return image, volume_data
+def segment(frame1, frame2):
+
+    image1, volume1 = drawLines(frame1, "testing/segmented_img.png")
+    image2, volume2 = drawLines(frame2, "testing/segmented_img1.png")
+
+    volumeData = [volume1, volume2]
+    ESV = max(volumeData)
+    EDV = max(volumeData)
+
+    EF = ((EDV-ESV)/ESV) * 100
+
+    EF = "EF: " + str(EF)
+    EDV = "EDV: " + str(EDV)
+    ESV = "ESV: " + str(ESV)
+
+    return image1, image2, EF, ESV, EDV
 
 import gradio as gr
 
 i = gr.inputs.Image(shape=(112, 112))
+p = gr.inputs.Image(shape=(112, 112))
+
 o = gr.outputs.Image()
+q = gr.outputs.Image()
 
 examples = [["testing/img1.jpg"], ["testing/img2.jpg"]]
 title = "Cardiac Function Evaluation"
 description = "Accurate assessment of ventricular vol-umes  and  cardiac  function  is  essentialfor  diagnosis  and  treatment  of  cardio-vascular  disease,   however  the  assess-ment  has  many  dependencies,  unreli-able assumptions in algorithm calcula-tion, and variability in accurate humanassessment.   We  developed  an  end-to-end  deep  learning  model  for  evaluat-ing  cardiac  function  by  1)  automatedidentification  of  end-systole  and  end-diastole, 2) segmentation of the left ven-tricle, and 3) algorithmic evaluation ofthe  cardiac  function.    Using  a  video-based deep learning model, end systolewas accurately identified an average ofwithin  150  ms  from  the  human  labelsand  end  diastole  was  accurately  iden-tified an average of within 167 ms fromthe human labels.  From the determinedframes, ventricular volumes were calcu-lated.  Even with the same ventriculartracing, slight perturbations in long axisposition resulted in variance in the esti-mates of ventricular volume up to 8.0%for  end  systole  and  5.2%  for  end  dias-tole.  Using standard volumetric formu-las, the ultimate calculated LVEF var-ied up to 5.8%.  Even with precise iden-tification of cardiac times and segmen-tation of cardiac structures, small varia-tions can compound to significantly im-pact eventual evaluations of ventricularfunction,  suggesting  limitations  in  theprecision of deep learning models for as-sessing cardiac function"
 thumbnail = "https://raw.githubusercontent.com/gradio-app/hub-echonet/master/thumbnail.png"
 
-gr.Interface(segment, i, [o, "text"], examples=examples, allow_flagging=False,
+gr.Interface(segment, [i, p], [o, q, "text", "text", "text"], examples=examples, allow_flagging=False,
 	title=title, description=description, thumbnail=thumbnail).launch()