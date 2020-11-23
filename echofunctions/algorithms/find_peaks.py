5043f8c2c43af82b325ed5033365062a43f220e5
ishanjainofficial <ishanjain42@gmail.com>
2020-11-23 00:05:55 -0800

    predictions against true sweeps and finding frame indices from given video

---
+++ echofunctions/algorithms/find_peaks.py
@@ -0,0 +1,65 @@
+"""Finding the ES and ED frames based on
+max and min volume calculations, respectively"""
+
+import pandas as pd
+from ast import literal_eval
+import os
+import cv2
+import loader
+import time
+import operator
+from algorithms import funcs
+
+def calculateVolumesForEachFrame(videoName, outputFolderName, method):
+    """Function to extract frames from input video file
+    and save them as separate frames in an output directory.
+    Args:
+        videoName: Input video name.
+        outputFolderName: Output directory to save the frames.
+    Returns:
+        None
+    """
+
+    root, _ = loader.dataModules()
+    volumeDict = {}
+
+    inputVideoPath = os.path.join(root, "segmented-videos", videoName)
+    outputPath = os.path.join(root, outputFolderName)
+    os.makedirs(outputPath, exist_ok=True) # creates folder for frames of given video
+    
+    cap = cv2.VideoCapture(inputVideoPath)
+    video_length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) - 1
+    
+    count = 0
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        # Crop
+        x1, y1, x2, y2 = 0, 0, 112, 112 # cropping coords and specs
+        crop = frame[x1:x2, y1:y2]
+
+        cv2.imwrite(os.path.join(outputPath, str((count+1)) + ".jpg"), crop)
+        count += 1
+        if (count > (video_length-1)):
+            cap.release()
+            break
+    
+    # Calculate Volumes
+    for frame in os.listdir(outputPath):
+      framePath = os.path.join(outputPath, frame)
+      volumes, *_ = funcs.calculateVolume(framePath, 20, 0, method)
+      volumeDict[framePath] = volumes[0]
+    
+    return volumeDict
+
+def returnPeaks(videoName="0X1BDEEC24D5FC570C.avi", outputFolderName="find_peaks", method="Method of Disks"):
+  volumeDict = calculateVolumesForEachFrame(videoName, outputFolderName, method)
+  
+  v=list(volumeDict.values())
+  k=list(volumeDict.keys())
+
+  ED_index = k[v.index(max(v))]
+  ES_index = k[v.index(min(v))]
+
+  return os.path.splitext(os.path.basename(ES_index))[0], os.path.splitext(os.path.basename(ED_index))[0]
--- echofunctions/plot_sweeps.py
+++ echofunctions/plot_sweeps.py
@@ -1,18 +1,71 @@
-"""Echonet Function Evaluation plotting
-the timing sweeps to visualize beats"""
+"""Echonet Function Evaluation to find
+EDV and ESV frame indices for given video"""
 
+from ast import literal_eval
+import loader
+from algorithms import find_peaks
+import tqdm
+from algorithms import funcs
+from algorithms import volume_tracings_calculations as tracings
+import config
+import pandas as pd
+import os
 
+def sortFrameVolumes(method="Method of Disks"):
+  _, df = loader.dataModules()
+  calculatedVolumeFromGroundTruth={}
+  
+  print("Calculating ground truth volumes from VolumeTracings")
+  for i in tqdm(range(len(df))):
+    videoName = df.iloc[i, 0]
+    
+    x1 = list(literal_eval(df.iloc[i, 2])) # x1 coords
+    y1 = list(literal_eval(df.iloc[i, 3])) # y1 coords
+    x2 = list(literal_eval(df.iloc[i, 4])) # x2 coords
+    y2 = list(literal_eval(df.iloc[i, 5])) # y2 coords
+    frame = df.iloc[i, 1] # frame number
+    
+    number = len(x1) - 1
 
-# Plot sizes (from Echonet segmentation.py)
-fig = plt.figure(figsize=(size.shape[0] / 50 * 1.5, 3))
-plt.scatter(np.arange(size.shape[0]) / 50, size, s=1)
-ylim = plt.ylim()
-for s in systole:
-  plt.plot(np.array([s, s]) / 50, ylim, linewidth=1)
-  plt.ylim(ylim)
-  plt.title(os.path.splitext(filename)[0])
-  plt.xlabel("Seconds")
-  plt.ylabel("Size (pixels)")
-  plt.tight_layout()
-  plt.savefig(os.path.join(output, "size", os.path.splitext(filename)[0] + ".pdf"))
-  plt.close(fig)
+    maxX1, maxY1, maxX2, maxY2, lowerInterceptAveragePoints, higherInterceptAveragePoints = tracings.calcParallelAndMaxPoints(x1, y1, x2, y2)
+
+    if number < 22:
+      if method == "Method of Disks":
+        ground_truth_volume = funcs.volumeMethodOfDisks(maxX1, maxY1, maxX2, maxY2, number, lowerInterceptAveragePoints, higherInterceptAveragePoints)
+      
+      if videoName not in calculatedVolumeFromGroundTruth:
+        calculatedVolumeFromGroundTruth[videoName] = []
+      
+      calculatedVolumeFromGroundTruth[videoName][frame] = ground_truth_volume
+  return calculatedVolumeFromGroundTruth
+
+def comparePredictedTimingAgainstTrue(fileName):
+  _, df = loader.dataModules()
+  dataList = []
+  trueVolumes = sortFrameVolumes()
+
+  for i in range(len(df)):
+    videoName = df.iloc[i, 0] # name of video
+    
+    # Get true frame index values
+    v=list(trueVolumes[videoName].values())
+    k=list(trueVolumes[videoName].keys())
+    true_EDV = k[v.index(max(v))]
+    true_ESV = k[v.index(min(v))]
+
+    # Gather predicted EDV and ESV timing frames
+    ESV_index, EDV_index = find_peaks.returnPeaks(videoName=videoName+".avi")
+
+    # Calculate Differences
+    differenceInEDV = abs(true_EDV - EDV_index)
+    differenceInESV = abs(true_ESV - ESV_index)
+
+    miniDict = {'Video Name': videoName, "Predicted ESV Frame": ESV_index, 'Predicted EDV Frame': EDV_index, 'True ESV Frame': true_ESV, 'True EDV Frame': true_EDV, 'Difference in ESV Frame': differenceInESV, 'Difference in EDV Frame': differenceInEDV}
+
+    dataList.append(miniDict)
+
+  df = pd.DataFrame(dataList)
+  export_path = os.path.join(config.CONFIG.DATA_DIR, fileName)
+  df.to_csv(export_path)
+
+comparePredictedTimingAgainstTrue("prediction-sweeps.csv")
