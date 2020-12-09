0171839b33ca48df72feca06a1ed59fb908f7498
ishanjainofficial <ishanjain42@gmail.com>
2020-12-09 11:29:19 -0800

    returns true frame indices to ease post processing

--- echofunctions/find_peaks.py
+++ echofunctions/find_peaks.py
@@ -9,7 +9,24 @@ import loader
 import time
 import operator
 from algorithms import funcs
+from algorithms import volume_tracings_calculations as tracings
 import glob
+
+def returnTrueFrames(frames, x1, y1, x2, y2):
+  volumesDict = {}
+  for i in [0, 1]:
+    number = len(x1[i]) - 1
+    frameNumber = frames[i]
+
+    maxX1, maxY1, maxX2, maxY2, lowerInterceptAveragePoints, higherInterceptAveragePoints = tracings.calcParallelAndMaxPoints(list(literal_eval(x1[i])), list(literal_eval(y1[i])), list(literal_eval(x2[i])), list(literal_eval(y2[i])))
+    ground_truth_volume = funcs.volumeMethodOfDisks(maxX1, maxY1, maxX2, maxY2, number, lowerInterceptAveragePoints, higherInterceptAveragePoints)
+    
+    volumesDict[frameNumber] = ground_truth_volume
+
+  ED = max(volumesDict.items(), key=operator.itemgetter(1))[0]
+  ES = min(volumesDict.items(), key=operator.itemgetter(1))[0]
+
+  return ED, ES
 
 def calculateVolumesForEachFrame(videoName, outputFolderName, method):
     """Function to extract frames from input video file
@@ -25,17 +42,24 @@ def calculateVolumesForEachFrame(videoName, outputFolderName, method):
     volumeDict = {}
 
     frameIndices = df[df['FileName']==videoName]['Frame'].values
-    frameIndices = [int(i) for i in frameIndices] 
-    
-    ES = min(frameIndices)
-    ED = max(frameIndices)
+    frameIndices = [int(i) for i in frameIndices]
+
+    x1 = df[df['FileName']==videoName]['X1'].values
+    x2 = df[df['FileName']==videoName]['X2'].values
+    y1 = df[df['FileName']==videoName]['Y1'].values
+    y2 = df[df['FileName']==videoName]['Y2'].values
+
+    true_ED, true_ES = returnTrueFrames(frameIndices, x1, y1, x2, y2) # returns ED and ES frame values
+    print(true_ED, true_ES)
 
     inputVideoPath = os.path.join(root, "segmented-videos", videoName + ".avi")
     outputPath = os.path.join(root, outputFolderName)
+    currentVideoPath = os.path.join(outputPath, videoName)
 
-    os.makedirs(outputPath, exist_ok=True) # creates folder for frames of given video
-        
-    clipNumber, clipEnd = ES-1, ED # clip start, clip end
+    os.makedirs(outputPath, exist_ok=True) # creates parent directory for storing frames
+    os.makedirs(currentVideoPath, exist_ok=True) # creates folder for each video under parent directory
+    
+    clipNumber, clipEnd = min([true_ES, true_ED]), max([true_ES, true_ED]) # clip start, clip end
     
     cap = cv2.VideoCapture(inputVideoPath)
     cap.set(clipNumber, clipEnd)
@@ -47,22 +71,22 @@ def calculateVolumesForEachFrame(videoName, outputFolderName, method):
       x1, y1, x2, y2 = 0, 0, 112, 112 # cropping coords and specs
       crop = frame[x1:x2, y1:y2]
       
-      cv2.imwrite(os.path.join(outputPath, str((clipNumber+1)) + ".jpg"), crop)
+      cv2.imwrite(os.path.join(outputPath, videoName, str((clipNumber)) + ".jpg"), crop)
       clipNumber += 1
       if (clipNumber is clipEnd):
         cap.release()
         break
     
     # Calculate Volumes
-    for frame in os.listdir(outputPath):
-      framePath = os.path.join(outputPath, frame)
+    for frame in os.listdir(os.path.join(outputPath, videoName)):
+      framePath = os.path.join(outputPath, videoName, frame)
       volumes, *_ = funcs.calculateVolume(framePath, 20, 0, method)
       volumeDict[os.path.splitext(frame)[0]] = volumes[0]
     
-    return volumeDict
+    return volumeDict, true_ES, true_ED
 
 def returnPeaks(videoName="0X1BDEEC24D5FC570C", outputFolderName="find_peaks", method="Method of Disks"):
-  volumeDict = calculateVolumesForEachFrame(videoName, outputFolderName, method)
+  volumeDict, true_ES, true_ED = calculateVolumesForEachFrame(videoName, outputFolderName, method)
   
   v=list(volumeDict.values())
   k=list(volumeDict.keys())
@@ -70,7 +94,4 @@ def returnPeaks(videoName="0X1BDEEC24D5FC570C", outputFolderName="find_peaks", m
   ED_index = k[v.index(max(v))]
   ES_index = k[v.index(min(v))]
 
-  return int(ES_index), int(ED_index)
-
-
-print(returnPeaks())
+  return [int(ES_index), int(ED_index)], [true_ES, true_ED]
